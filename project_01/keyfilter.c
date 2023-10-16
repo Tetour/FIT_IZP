5b73c1421011628cce948ad3ce2ff6798a1dfad1
Tetour <vrska.adam@protonmail.com>
2023-10-16 14:50:24 +0200

    Add partial solution of project

--- project_01/keyfilter.c
+++ project_01/keyfilter.c
@@ -4,19 +4,25 @@
 #include <ctype.h>
 
 
-#define DEBUG_ENABLE 1  // flag enabling debug output, 0 ... disabled, 1 ... enabled
+#define DEBUG_ENABLE        1  // flag enabling debug output, 0 ... disabled, 1 ... enabled
+#define MAX_ADDRESS_COUNT   100
+#define MAX_ADDRESS_LENGTH  100
+#define MAX_CHARACTER_COUNT 26
+#define ADDRESS_NOT_FOUND   (MAX_ADDRESS_COUNT + 1)
 
 
-size_t address_total_count = 0;
-size_t address_found_count = 0;
-char   address_array[100][100] = {0};
-int    enabled_chars[26] = {0};
+int     address_total_count = 0;
+int     address_found_index = ADDRESS_NOT_FOUND;
+int     enabled_chars[MAX_CHARACTER_COUNT] = {0};
+char    address_array[MAX_ADDRESS_COUNT][MAX_ADDRESS_LENGTH] = {0};
 
 const char *prefix_string = "";
 const char *address_string = "";
 
 void set_address_count(const char *address_string);
 void set_address_array(const char *address_string);
+void analyze_one_address(int address_index);
+void print_results();
 
 
 int main(int argc, char const *argv[])
@@ -43,17 +49,21 @@ int main(int argc, char const *argv[])
 
     #if DEBUG_ENABLE
         printf("Input text: %s\n", prefix_string);
-        printf("Address count: %ld\n", address_total_count);
+        printf("Address count: %d\n", address_total_count);
         printf("Address list:\n%s\n\n", address_string);
     #endif
 
     set_address_array(address_string);
 
+    analyze_one_address(0);
+
+    print_results();
 
     return 0;
 }
 
-void set_address_count(const char *address_string) {
+void set_address_count(const char *address_string)
+{
     if (address_string == NULL) {
         printf("ERROR: Implementation error, wrong argument in set_address_count funtion.\n");
         exit(1);
@@ -64,7 +74,7 @@ void set_address_count(const char *address_string) {
         exit(1);
     }
 
-    size_t char_index = 0;
+    int char_index = 0;
     char ch = address_string[char_index];
 
     while (ch != '\0') {
@@ -77,15 +87,16 @@ void set_address_count(const char *address_string) {
     address_total_count++;
 }
 
-void set_address_array(const char *address_string){
+void set_address_array(const char *address_string)
+{
     if (address_string == NULL) {
         printf("ERROR: Implementation error, wrong argument in set_address_array funtion.\n");
         exit(1);
     }
 
-    size_t address_index_array = 0;
-    size_t char_index_string = 0;
-    size_t char_index_array = 0;
+    int address_index_array = 0;
+    int char_index_string = 0;
+    int char_index_array = 0;
     char ch = address_string[char_index_string];
 
     while (ch != '\0') {
@@ -110,27 +121,47 @@ void set_address_array(const char *address_string){
     }
 }
 
-void analyze_one_address(char *address){
+void analyze_one_address(int address_index)
+{
 
-    size_t char_index_address = 0;
-    size_t char_index_prefix = 0;
-    
-    char ch = address[char_index_address];
-    
-    for (size_t i = 0; i < 100; i++) {
-        
+    const char * address = address_array[address_index];
 
-        if (prefix_string[char_index_prefix] == '\0') {
-
-        } else if (address[char_index_address] == '\0') {
-
-        }
-
-        if (address[char_index_address] != prefix_string[char_index_prefix]) {
-            break;
-        } 
+    if (strcmp(prefix_string, address) == 0) {
+        address_found_index = address_index;
+    } else {
+        if (strncmp(prefix_string, address, strlen(prefix_string)) == 0) {
+            char ch = address[strlen(prefix_string) + 1];
+            enabled_chars[ch - 'a'] = 1;
+            address_found_index = address_index;
+        }   
     }
-
-    
 }
 
+void print_results()
+{
+    char results[MAX_CHARACTER_COUNT * 2] = {0};
+    int num_enabled_chars = 0;
+
+    for (int i = 0; i < MAX_CHARACTER_COUNT; i++) {
+        if (enabled_chars[i] == 1) {
+            num_enabled_chars++;
+        }
+    }
+
+    if ((num_enabled_chars == 0) && (address_found_index == ADDRESS_NOT_FOUND)) {
+        printf("Not found\n");
+    } else if (num_enabled_chars == 1) {
+        printf("Found: %s\n", address_array[address_found_index]);
+    } else if (num_enabled_chars >= 2) {
+        int j = 0;
+        for (int i = 0; i < MAX_CHARACTER_COUNT; i++) {
+            if (enabled_chars[i] == 1) {
+                results[j] = (char)(i + 'A');
+                j++;
+            }
+        }
+        printf("Enable: %s", results);
+    } else if ((num_enabled_chars == 0) && (address_found_index != ADDRESS_NOT_FOUND)) {
+        printf("Found: %s\n", address_array[address_found_index]);
+    }
+}
